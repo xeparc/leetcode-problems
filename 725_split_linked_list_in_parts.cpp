3733e7836ae2e867b120f38c4d6dd90a451f33b0
xeparc <xeparc@gmail.com>
2021-10-03 23:18:19 +0300

    Added C++ includes

--- 725_split_linked_list_in_parts.cpp
+++ 725_split_linked_list_in_parts.cpp
@@ -42,17 +42,21 @@
 // 1 <= k <= 50
 
 
+#include <vector>
+
+using namespace std;
 
 /**
  * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
  */
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
 class Solution {
 public:
     vector<ListNode*> splitListToParts(ListNode* head, int k) {
