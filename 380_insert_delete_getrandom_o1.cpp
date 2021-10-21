e70cdabc19eaa1bc5709e8da9c9916d5eec01cdd
Stefan Petrov <stefan.petrov@danlex.bg>
2021-10-21 15:37:41 +0300

    380. Insert Delete GetRandom O(1)

---
+++ 380_insert_delete_getrandom_o1.cpp
@@ -0,0 +1,107 @@
+// ///////////////////////////////////////////////////////////////////////////
+//
+// 380. Insert Delete GetRandom O(1)
+// https://leetcode.com/problems/insert-delete-getrandom-o1/
+//
+// ***************************************************************************
+//
+//
+// Implement the RandomizedSet class:
+//
+// * RandomizedSet()
+//     Initializes the RandomizedSet object.
+// * bool insert(int val)
+//     Inserts an item val into the set if not present.
+//     Returns true if the item was not present, false otherwise.
+// * bool remove(int val)
+//     Removes an item val from the set if present.
+//     Returns true if the item was present, false otherwise.
+// * int getRandom()
+//     Returns a random element from the current set of elements
+//     (it's guaranteed that at least one element exists when this
+//     method is called). Each element must have the same probability
+//     of being returned.
+//
+// You must implement the functions of the class such that each function
+// works in average O(1) time complexity.
+//
+//
+// Example 1:
+//
+// Input
+// ["RandomizedSet", "insert", "remove", "insert", "getRandom", "remove", "insert", "getRandom"]
+// [[], [1], [2], [2], [], [1], [2], []]
+// Output
+// [null, true, false, true, 2, true, false, 2]
+//
+// Explanation
+// RandomizedSet randomizedSet = new RandomizedSet();
+// randomizedSet.insert(1); // Inserts 1 to the set. Returns true as 1 was inserted successfully.
+// randomizedSet.remove(2); // Returns false as 2 does not exist in the set.
+// randomizedSet.insert(2); // Inserts 2 to the set, returns true. Set now contains [1,2].
+// randomizedSet.getRandom(); // getRandom() should return either 1 or 2 randomly.
+// randomizedSet.remove(1); // Removes 1 from the set, returns true. Set now contains [2].
+// randomizedSet.insert(2); // 2 was already in the set, so return false.
+// randomizedSet.getRandom(); // Since 2 is the only number in the set, getRandom() will always return 2.
+//
+//
+// Constraints:
+//
+// * -2^31 <= val <= 2^31 - 1
+// * At most 2 * 10^5 calls will be made to insert, remove, and getRandom.
+// * There will be at least one element in the data structure when getRandom is called.
+
+
+
+#include <random>
+#include <vector>
+#include <unordered_map>
+
+using namespace std;
+
+
+class RandomizedSet {
+public:
+    RandomizedSet() : _set(), _array(), _size{0}, _engine() {}
+
+    bool insert(int val) {
+        if (_set.count(val) == 0){
+            _array.push_back(val);
+            _set[val] = _size++;
+            return true;
+        }
+        return false;
+    }
+
+    bool remove(int val) {
+        if (_set.count(val)){
+            size_t i = _set[val];
+            int last = _array[--_size];
+            _array[i] = last;
+            _set[last] = i;
+            _array.pop_back();
+            _set.erase(val);
+            return true;
+        }
+        return false;
+    }
+
+    int getRandom() {
+        uniform_int_distribution<int> uniform_dist(0, _size - 1);
+        return _array[uniform_dist(_engine)];
+    }
+
+private:
+    unordered_map<int, int> _set;
+    vector<int> _array;
+    size_t _size;
+    default_random_engine _engine;
+};
+
+/**
+ * Your RandomizedSet object will be instantiated and called as such:
+ * RandomizedSet* obj = new RandomizedSet();
+ * bool param_1 = obj->insert(val);
+ * bool param_2 = obj->remove(val);
+ * int param_3 = obj->getRandom();
+ */
