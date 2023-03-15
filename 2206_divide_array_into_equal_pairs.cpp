c587425f07633ba47387e516d396f25390f88ec0
xeparc <xeparc@gmail.com>
2023-03-15 20:59:52 +0200

    2206. Divide Array Into Equal Pairs

---
+++ 2206_divide_array_into_equal_pairs.cpp
@@ -0,0 +1,53 @@
+// ///////////////////////////////////////////////////////////////////////////
+//
+// 2206. Divide Array Into Equal Pairs
+// https://leetcode.com/problems/divide-array-into-equal-pairs/description/
+// 
+// ***************************************************************************
+//
+//
+// You are given an integer array nums consisting of 2 * n integers.
+// You need to divide nums into n pairs such that:
+//     Each element belongs to exactly one pair.
+//     The elements present in a pair are equal.
+// Return true if nums can be divided into n pairs, otherwise return false.
+// 
+// 
+// Example 1:
+// Input: nums = [3,2,3,2,2,2]
+// Output: true
+// Explanation: 
+// There are 6 elements in nums, so they should be divided into 6 / 2 = 3 pairs.
+// If nums is divided into the pairs (2, 2), (3, 3), and (2, 2), it will satisfy
+// all the conditions.
+// 
+// Example 2:
+// Input: nums = [1,2,3,4]
+// Output: false
+// Explanation: 
+// There is no way to divide nums into 4 / 2 = 2 pairs such that the pairs
+// satisfy every condition.
+// 
+// Constraints:
+//     * nums.length == 2 * n
+//     * 1 <= n <= 500
+//     * 1 <= nums[i] <= 500
+
+
+
+#include <array>
+#include <vector>
+using namespace std;
+
+
+class Solution {
+public:
+    bool divideArray(vector<int>& nums) {
+        std::array<int, 500> counts = {0,};
+        for (int x : nums)
+            counts[x-1]++;
+        for (int x : counts)
+            if (x % 2) return false;
+        return true;
+    }
+};
